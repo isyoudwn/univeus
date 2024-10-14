d062e2f65560553fe0073c08f782d02b37e0d93b
임채연 <dlacodus0407@gmail.com>
2024-10-14 15:39:18 +0900

    init : 초기 구성 파일 업로드

---
+++ src/main/java/com/example/univeus/UniveusApplication.java
@@ -0,0 +1,13 @@
+package com.example.univeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniveusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniveusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/univeus/UniveusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.univeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniveusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
