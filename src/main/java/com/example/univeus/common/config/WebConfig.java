5be38a2966ae15e5d182417a0a6447304e49a0b1
임채연 <dlacodus0407@gmail.com>
2025-01-12 17:32:52 +0900

    feat : add resolver setting in webConfig file
    - resolver를 webConfig에 추가하여, 사용을 enable한다

--- src/main/java/com/example/univeus/common/config/WebConfig.java
+++ src/main/java/com/example/univeus/common/config/WebConfig.java
@@ -1,11 +1,24 @@
 package com.example.univeus.common.config;
 
+import com.example.univeus.common.resolver.AuthenticationResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationResolver authenticationResolver;
+
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationResolver);
+    }
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // CORS 정책 설정 (도메인과 credentials을 허용)
